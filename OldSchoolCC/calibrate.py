1c8972307b5477226b6b70b172d502b3effa8ae1
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2020-03-18 12:48:11 +0100

    Old School Camera Calibration

--- OldSchoolCC/calibrate.py
+++ OldSchoolCC/calibrate.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from tqdm import tqdm
-
+from collections import Iterable
 
 def calibrate_camera(gray_imgs, pattern_size, win_size=(10, 10), zero_zone=(-1, -1), criteria=None):
     """
@@ -10,28 +10,33 @@ def calibrate_camera(gray_imgs, pattern_size, win_size=(10, 10), zero_zone=(-1, 
     10), zero_zone=(-1, -1), criteria=None)
 
     Arguments:
-    :param gray_imgs: Array there contains the images with chessboards. The images has to be in grayscale colorspace
-    :param pattern_size: Number of inner corners per a chessboard row and column
-    :param win_size: Half of the side length of the search window
-    :param zero_zone:Half of the size of the dead region in the middle of the search zone over which the summation
-    in the formula below is not done. It is used sometimes to avoid possible singularities of
-    the autocorrelation matrix. The value of (-1,-1) indicates that there is no such a size.
-    :param criteria: Criteria for termination of the iterative process of corner refinement. That is,
-    the process of corner position refinement stops either after criteria.maxCount iterations or when
-    the corner position moves by less than criteria.epsilon on some iteration.
-    If None the criteria will be set to (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        gray_imgs (iterable): Array there contains the images with chessboards. The images has to be in grayscale colorspace
+        pattern_size (tuple): Number of inner corners per a chessboard row and column
+        win_size (tuple): Half of the side length of the search window
+        zero_zone (tuple): Half of the size of the dead region in the middle of the search zone over which the summation
+            in the formula below is not done. It is used sometimes to avoid possible singularities of
+            the autocorrelation matrix. The value of (-1,-1) indicates that there is no such a size.
+        criteria (tuple): Criteria for termination of the iterative process of corner refinement. That is,
+            the process of corner position refinement stops either after criteria.maxCount iterations or when
+            the corner position moves by less than criteria.epsilon on some iteration.
+            If None the criteria will be set to (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     Returns:
-    :return ret: The RMS re-projection error in pixels
-    :return camera_matrix: Output 3x3 floating-point camera matrix
-    [[fx, 0,  cx],
-     [0,  fy, cy],
-     [0,  0,  1]]
-    :return dist_coeffs: Output vector of distortion coefficients
-    (8 coefficients, 6 zeros = 15 values (k1, k2, p1, p2, k3, k4, k5, k6, 0, 0, 0, 0, 0, 0))
-    :return rvecs: Rotation vectors estimated for each pattern view
-    :return tvecs: Translation vectors estimated for each pattern view
+        ret (float): The RMS re-projection error in pixels
+        camera_matrix (ndarray): Output 3x3 floating-point camera matrix
+            [[fx, 0,  cx],
+             [0,  fy, cy],
+             [0,  0,  1]]
+        dist_coeffs (ndarray): Output vector of distortion coefficients
+            (8 coefficients, 6 zeros = 15 values (k1, k2, p1, p2, k3, k4, k5, k6, 0, 0, 0, 0, 0, 0))
+        rvecs (ndarray): Rotation vectors estimated for each pattern view
+        tvecs (ndarray): Translation vectors estimated for each pattern view
     """
+    assert isinstance(gray_imgs, Iterable), "gray_imgs has to be a iterable there consists of the grayscale images"
+    assert len(pattern_size) == 2 and isinstance(pattern_size, tuple), "pattern_size has to be a tuple of length 2"
+    assert len(win_size) == 2 and isinstance(win_size, tuple), "win_size has to be a tuple of length 2"
+    assert len(zero_zone) == 2 and isinstance(zero_zone, tuple), "zero_zone has to be a tuple of length 2"
+
     if criteria is None:
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
