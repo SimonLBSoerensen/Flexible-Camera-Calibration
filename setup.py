496c809b26f4d07fab4e5c1f17e7f96f7f6e8a64
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2020-03-20 11:04:51 +0100

    FCC setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='fcc',
+     name='flexible-camera-calibration',
      version='0.1',
      author="Simon L. B. Sørensen, Oliver H. Vea, Malte N. Jensen and Jakob Yde-Madsen",
      author_email="simso16@student.sdu.dk",
