b1c3eb737c5bf28f2026f15a3b9c732a5bf0053a
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2020-04-26 18:07:24 +0200

    BundleAdjustment initialization

--- flexiblecc/BundleAdjustment/bundle_adjustment.py
+++ flexiblecc/BundleAdjustment/bundle_adjustment.py
@@ -4,63 +4,101 @@ import time, datetime
 import json
 from matplotlib import pyplot as plt
 from flexiblecc.BSpline.central_model import CentralModel, fit_central_model
+from flexiblecc.BundleAdjustment import initialization
 from scipy.optimize import least_squares
 from scipy.sparse import lil_matrix, csr_matrix
 from socket import gethostname
 from tqdm import tqdm
 from sys import argv
 
-def forward_project(n_images, rvecs, tvecs, grid_width, grid_height, n_grid_points):
 
-    #TODO: make global, don't run all the time
+def forward_project(n_images, rvecs, tvecs, grid_width, grid_height, n_grid_points):
+    # TODO: make global, don't run all the time
     grid = np.zeros((n_grid_points, 3))
-    meshgrid = np.transpose(np.meshgrid(np.arange(grid_width),np.arange(grid_height)))
+    meshgrid = np.transpose(np.meshgrid(np.arange(grid_width), np.arange(grid_height)))
 
     meshgrid = np.reshape(meshgrid, (n_grid_points, 2))
 
     grid[:, :-1] = meshgrid
-    grid = np.reshape(grid, grid.shape+(1,))
+    grid = np.reshape(grid, grid.shape + (1,))
 
     rays = np.ndarray((n_images, n_grid_points, 3, 1))
     for i, r, t in zip(range(n_images), rvecs, tvecs):
         rotation_matrix = cv2.Rodrigues(r)[0]
         for j in range(n_grid_points):
-            rays[i,j] = np.dot(rotation_matrix.T, grid[j]-t)
-            
-            #Normalize each ray
-            rays[i,j] = np.divide(rays[i,j], np.linalg.norm(rays[i,j])) 
+            rays[i, j] = np.dot(rotation_matrix.T, grid[j] - t)
+
+            # Normalize each ray
+            rays[i, j] = np.divide(rays[i, j], np.linalg.norm(rays[i, j]))
 
     return rays
 
+
 def calc_residuals(parameters, p, n_images, points_2D, grid_width, grid_height, n_grid_points, image_size, cm_shape):
     spline_grid = parameters[:np.prod(cm_shape)].reshape(cm_shape)
-    rvecs = parameters[np.prod(cm_shape):][:n_images*3].reshape((94,3,1))
-    tvecs = parameters[np.prod(cm_shape) + n_images*3 :].reshape((94,3,1))
+    rvecs = parameters[np.prod(cm_shape):][:n_images * 3].reshape((94, 3, 1))
+    tvecs = parameters[np.prod(cm_shape) + n_images * 3:].reshape((94, 3, 1))
 
     b_spline_object = CentralModel(
-        image_dimensions=image_size, 
-        grid_dimensions=image_size, 
-        control_points=spline_grid, 
+        image_dimensions=image_size,
+        grid_dimensions=image_size,
+        control_points=spline_grid,
         order=p['cm_order'],
         knot_method=p['cm_knot_method'],
         min_basis_value=p['cm_min_basis_value'],
         end_divergence=p['cm_end_divergence'])
 
-    #Calculate forward projected rays
+    # Calculate forward projected rays
     forward_projected_rays = forward_project(n_images, rvecs, tvecs, grid_width, grid_height, n_grid_points).ravel()
 
-    #Calculate backward projected rays (b_spline)
+    # Calculate backward projected rays (b_spline)
     model_rays = np.ndarray((n_images, n_grid_points, 3))
     for i, image_points in zip(range(n_images), points_2D):
         for j, point_2D in zip(range(n_grid_points), image_points):
-            model_rays[i,j] = b_spline_object.sample(point_2D[0,0], point_2D[0,1])
+            model_rays[i, j] = b_spline_object.sample(point_2D[0, 0], point_2D[0, 1])
 
     model_rays = model_rays.ravel()
 
     return forward_projected_rays - model_rays
 
 
-def grid_creation():
+def get_sparsity_matrix(n_images, n_points, b_spline_shape, points_2D, image_size):
+    m = n_images * n_points * 3  # total number of residuals
+    n_spline_params = np.prod(b_spline_shape)  # number of b-spline parameters
+    n_rvecs_params = n_images * 3  # number of rotation vector parameters
+    n_tvecs_params = n_images * 3  # number of translation vector parameters
+    n = n_spline_params + n_rvecs_params + n_tvecs_params  # total number of parameters
+    A = lil_matrix((m, n), dtype=int)  # sparse matrix describing the connection between parameters and residuals
+
+    residuals_pr_image = np.arange(n_points * 3)
+
+    # Fill out sparsity matrix for b-spline parameters.
+    spline_grid = np.ndarray((b_spline_shape))
+    b_spline_object = CentralModel(image_size, image_size, spline_grid, 2)
+
+    points_2D = points_2D.reshape((-1, 2))
+    for i in range(len(points_2D)):
+        control_points = b_spline_object.active_control_points(points_2D[i, 0], points_2D[i, 1])
+        control_points = control_points.reshape((-1, 2))
+
+        temp = np.zeros(b_spline_shape)
+        for point in control_points:
+            temp[int(point[0]), int(point[1])] = np.array([1, 1, 1])
+
+        A[i * 3:i * 3 + 3, :n_spline_params] = np.vstack([temp.ravel()] * 3)
+
+    # Fill out sparsity matrix for rvecs
+    for i in range(n_rvecs_params):
+        A[n_points * 3 * int(i / 3) + residuals_pr_image, n_spline_params + i] = 1
+
+    # Fill out sparsity matrix for tvecs
+    for i in range(n_tvecs_params):
+        A[n_points * 3 * int(i / 3) + residuals_pr_image, n_spline_params + n_rvecs_params + i] = 1
+
+    return A
+
+
+def old_grid_creation():
     step = 400
 
     border = 0
@@ -68,14 +106,14 @@ def grid_creation():
     image_height = 1200
 
     x = np.arange(
-        start=-border, 
-        stop=image_width + border + step, 
+        start=-border,
+        stop=image_width + border + step,
         step=step,
         dtype=float)
 
     y = np.arange(
-        start=-border, 
-        stop=image_height + border + step, 
+        start=-border,
+        stop=image_height + border + step,
         step=step,
         dtype=float)
 
@@ -88,16 +126,16 @@ def grid_creation():
 
     K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
 
-    k1, k2, p1, p2, k3, k4, k5, k6, *_ = 2.03288270e+00,  4.52548448e+01, -7.73226736e-03, 1.80779861e-02, -1.47886929e+02,  2.09683329e+00,  4.65342697e+01, -1.51419436e+02, -1.83082177e-02, -1.92297600e-03, 9.86103858e-03, 7.83822797e-03, 6.18311558e-03, 3.67951499e-02
+    k1, k2, p1, p2, k3, k4, k5, k6, *_ = 2.03288270e+00, 4.52548448e+01, -7.73226736e-03, 1.80779861e-02, -1.47886929e+02, 2.09683329e+00, 4.65342697e+01, -1.51419436e+02, -1.83082177e-02, -1.92297600e-03, 9.86103858e-03, 7.83822797e-03, 6.18311558e-03, 3.67951499e-02
 
     distCoeffs = np.array([k1, k2, p1, p2, k3, k4, k5, k6])
 
     R = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     t = np.array([0, 0, 0])
 
-    Rt = np.ndarray((3,4))
-    Rt[:,:3] = R
-    Rt[:,3] = t
+    Rt = np.ndarray((3, 4))
+    Rt[:, :3] = R
+    Rt[:, 3] = t
 
     P = np.dot(K, Rt)
 
@@ -105,48 +143,13 @@ def grid_creation():
         src=src,
         cameraMatrix=K,
         distCoeffs=distCoeffs,
-        P=P)[:,0,:]
-        
+        P=P)[:, 0, :]
+
     Kinv = np.linalg.inv(K)
 
-    projected_pts = np.array([np.dot(Kinv, np.array([[pt[0],], [pt[1],], [1,]])) for pt in dst]).reshape((-1,3))
+    projected_pts = np.array([np.dot(Kinv, np.array([[pt[0], ], [pt[1], ], [1, ]])) for pt in dst]).reshape((-1, 3))
 
     return projected_pts
-
-def get_sparsity_matrix(n_images, n_points, b_spline_shape, points_2D, image_size):
-    m = n_images * n_points * 3                             # total number of residuals
-    n_spline_params = np.prod(b_spline_shape)               # number of b-spline parameters
-    n_rvecs_params = n_images * 3                           # number of rotation vector parameters
-    n_tvecs_params = n_images * 3                           # number of translation vector parameters
-    n = n_spline_params + n_rvecs_params + n_tvecs_params   # total number of parameters
-    A = lil_matrix((m, n), dtype=int)                       # sparse matrix describing the connection between parameters and residuals
-
-    residuals_pr_image = np.arange(n_points*3)
-
-    #Fill out sparsity matrix for b-spline parameters.
-    spline_grid = np.ndarray((b_spline_shape))
-    b_spline_object = CentralModel(image_size, image_size, spline_grid, 2)
-
-    points_2D = points_2D.reshape((-1, 2))
-    for i in range(len(points_2D)):
-        control_points = b_spline_object.active_control_points(points_2D[i,0], points_2D[i,1])
-        control_points = control_points.reshape((-1, 2))
-        
-        temp = np.zeros(b_spline_shape)
-        for point in control_points:
-            temp[int(point[0]), int(point[1])] = np.array([1,1,1])
-            
-        A[i*3:i*3+3, :n_spline_params] = np.vstack([temp.ravel()] * 3)
-
-    #Fill out sparsity matrix for rvecs
-    for i in range(n_rvecs_params):
-        A[n_points*3*int(i/3)+residuals_pr_image, n_spline_params + i] = 1
-
-    #Fill out sparsity matrix for tvecs
-    for i in range(n_tvecs_params):
-        A[n_points*3*int(i/3)+residuals_pr_image, n_spline_params + n_rvecs_params + i] = 1
-
-    return A
 
 
 def bundle_adjustment(p):
@@ -156,62 +159,66 @@ def bundle_adjustment(p):
 
     n_images = len(rvecs)
 
-    start_grid = grid_creation()
+    # Ligger image_shape, cameraMatrix, distCoeffs i ba_initialization_file?
+    # start_grid = initialization.grid_creation(image_shape, cameraMatrix, distCoeffs, border=0, step=400)
+
+    # For at koden stadig virker har jeg insat den gamel igen
+    start_grid = old_grid_creation()
 
     start_grid = np.divide(start_grid, np.linalg.norm(start_grid, axis=1).reshape((start_grid.shape[0], 1)))
 
     if p['cm_fit_control_points']:
         cm, _ = fit_central_model(start_grid.reshape(p['cm_shape']),
-            image_dimensions=p['image_size'], 
-            grid_dimensions=p['image_size'], 
-            order=p['cm_order'],
-            knot_method=p['cm_knot_method'],
-            min_basis_value=p['cm_min_basis_value'],
-            end_divergence=p['cm_end_divergence'],
-            verbose=p['ls_verbose'])
-        start_grid = cm.a.reshape((-1,3))
+                                  image_dimensions=p['image_size'],
+                                  grid_dimensions=p['image_size'],
+                                  order=p['cm_order'],
+                                  knot_method=p['cm_knot_method'],
+                                  min_basis_value=p['cm_min_basis_value'],
+                                  end_divergence=p['cm_end_divergence'],
+                                  verbose=p['ls_verbose'])
+        start_grid = cm.a.reshape((-1, 3))
 
     ba_params = np.hstack((start_grid.ravel(), rvecs.ravel(), tvecs.ravel()))
 
     residuals_init = calc_residuals(parameters=ba_params,
-        p=p,
-        n_images=n_images, 
-        points_2D=points_2D, 
-        grid_width=p['checkerboard_shape'][0], 
-        grid_height=p['checkerboard_shape'][1], 
-        n_grid_points=np.prod(p['checkerboard_shape']),
-        image_size=p['image_size'],
-        cm_shape=p['cm_shape'])
+                                    p=p,
+                                    n_images=n_images,
+                                    points_2D=points_2D,
+                                    grid_width=p['checkerboard_shape'][0],
+                                    grid_height=p['checkerboard_shape'][1],
+                                    n_grid_points=np.prod(p['checkerboard_shape']),
+                                    image_size=p['image_size'],
+                                    cm_shape=p['cm_shape'])
 
     A = None
     if p['ls_sparsity']:
         A = get_sparsity_matrix(n_images, points_2D.shape[1], p['cm_shape'], points_2D, p['image_size'])
-        #plt.spy(A, aspect='auto')
-        #plt.show()
-        
+        # plt.spy(A, aspect='auto')
+        # plt.show()
+
     res = least_squares(
-        fun=calc_residuals, 
-        x0=ba_params, 
-        jac_sparsity=A, 
-        verbose=p['ls_verbose'], 
-        x_scale='jac', 
-        ftol=p['ls_ftol'], 
+        fun=calc_residuals,
+        x0=ba_params,
+        jac_sparsity=A,
+        verbose=p['ls_verbose'],
+        x_scale='jac',
+        ftol=p['ls_ftol'],
         gtol=p['ls_gtol'],
-        method=p['ls_method'], 
+        method=p['ls_method'],
         args=(p,
-            n_images, 
-            points_2D, 
-            p['checkerboard_shape'][0], 
-            p['checkerboard_shape'][1], 
-            np.prod(p['checkerboard_shape']),
-            p['image_size'],
-            p['cm_shape']))
-    
+              n_images,
+              points_2D,
+              p['checkerboard_shape'][0],
+              p['checkerboard_shape'][1],
+              np.prod(p['checkerboard_shape']),
+              p['image_size'],
+              p['cm_shape']))
+
     duration = time.time() - start_time
-    
+
     spline_grid = res.x[:np.prod(p['cm_shape'])].reshape(p['cm_shape'])
-    rvecs = res.x[np.prod(p['cm_shape']):][:n_images*3].reshape((94,3,1))
-    tvecs = res.x[np.prod(p['cm_shape']) + n_images*3 :].reshape((94,3,1))
+    rvecs = res.x[np.prod(p['cm_shape']):][:n_images * 3].reshape((94, 3, 1))
+    tvecs = res.x[np.prod(p['cm_shape']) + n_images * 3:].reshape((94, 3, 1))
     res['initial_residuals'] = residuals_init
     res['duration'] = duration
     res['start_grid'] = start_grid
@@ -220,12 +227,13 @@ def bundle_adjustment(p):
     res['tvecs'] = tvecs
     return res
 
+
 if __name__ == '__main__':
     parameters = {
         'image_size': (1600, 1200),
         'checkerboard_shape': (12, 12),
         'cm_dimensions': (1600, 1200),
-        'cm_shape': (5,4,3),
+        'cm_shape': (5, 4, 3),
         'cm_order': 3,
         'cm_fit_control_points': True,
         'cm_knot_method': 'open_uniform',
@@ -256,7 +264,7 @@ if __name__ == '__main__':
 
     if parameters['seed'] != None:
         np.random.seed(parameters['seed'])
-        
+
     res = bundle_adjustment(parameters)
 
     for key in res:
@@ -264,9 +272,7 @@ if __name__ == '__main__':
             res[key] = res[key].toarray()
         if isinstance(res[key], np.ndarray):
             res[key] = res[key].tolist()
-    
-    filename = 'json/' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') +'.json'
+
+    filename = 'json/' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.json'
     with open(filename, 'w') as f:
         json.dump({'parameters': parameters, 'results': res}, f, ensure_ascii=False, indent=4)
-
-        
